66a1ba20cb4d84bc26e3ddfa96f1d471c51083a4
GabrielMani15 <114151976+GabrielMani15@users.noreply.github.com>
2024-05-27 15:15:21 +0000

    V1

---
+++ builder.ts
@@ -0,0 +1,17 @@
+/* 
+    Wepsite builder
+*/
+
+/* 
+    Fields to edit.
+    Title, title is gonna be displayed as the name of the wepsite.
+    Fetch data, here you can add link to fetch data.
+*/
+
+const app = document.getElementById("#app")
+
+class Builder{
+    constructor() {
+        
+    }
+}
